92fe83c04c35c7b257dea6123aa8ac55c228d131
yatsun <yatsunlam25@gmail.com>
2021-09-15 01:56:09 -0400

    commit tree c942b5a8409a3ed6e666a5ef48c2c8e7a2fd2404

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+function map(a, callback){
+    // let arr =  [];
+    // i = 0;
+    // while (i < a.length) {
+    //     let b = a[i];
+    //     arr.push(callback(b));
+    //     i++;
+    // }
+    // return arr
+    const arr = [];
+    for (let i = 0; i < a.length; i++){
+        const b = a[i];
+        arr.push(callback(b));
+    }
+    return arr
+}
+
+function reduce(a, callback, startPoint){
+    let sum = 0;
+    if (startPoint) {
+        sum = startPoint;
+        for (let i = 0; i < a.length; i++){
+            sum = callback(a[i], sum);
+        }
+        return sum;
+    } else {
+        sum = a[0];
+        for (let i = 1; i < a.length; i++){
+            sum = callback(a[i], sum);
+        }
+        return sum
+    }
+}
